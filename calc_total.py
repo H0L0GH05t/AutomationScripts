ccd158279491dfc531551fd541844cce817cce8f
Arin Kase <7828330+H0L0GH05t@users.noreply.github.com>
2025-05-23 14:16:37 -0600

    Made file reading dynamic
    Added folder to read from so we now take the newest receipt file from the folder.
    Added gitignore to ignore actual receipt files.

--- calc_total.py
+++ calc_total.py
@@ -1,17 +1,31 @@
 from decimal import *
+import os, glob
 
-# TODO: make filename dynamic
+# FILENAME = "receipts/insert_filename_here.txt" # override
 
-FILENAME = "05-12-25.txt"
+RECEIPTS_FOLDER = "receipts"
 
-def calculate_total():
+def find_newest_receipt():
+    list_of_files = glob.glob(os.path.join(RECEIPTS_FOLDER, '*'))
+    if not list_of_files:
+        return None
+
+    newest_receipt = max(list_of_files, key=os.path.getmtime)
+
+    print(f"Calculating total for '{newest_receipt}'...\n")
+    return newest_receipt
+
+def calculate_total(file_path):
     total = Decimal(0.00)
-    with open(FILENAME, "r") as file:
+    with open(file_path, "r") as file:
         data = file.readlines()
         for line in data:
             print(f"Adding {line}")
             total += Decimal(line)
     return total
 
-total_amt = calculate_total()
+newest_receipt_path = find_newest_receipt()
+total_amt = calculate_total(newest_receipt_path)
+# total_amt = calculate_total(FILENAME) #override
+
 print(f"You owe {total_amt/2} of the total {total_amt}")
